5f4fe2ada35dc58ee5e5d4241f60f43fe84a213f
Sofie <sov.002@edu.zealand.dk>
2026-05-19 00:26:43 +0200

    router added in App.jsx, & Cart-page added

---
+++ makeupstore-context-api/src/Pages/Cart.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Cart = () =>{
+    return(
+        <div className="container justify-content-center align-items-center mt-5 ">
+        <div className="row row-cols-1 row-cols-md-3 g-4 p-4 mt-3">
+            <h1>Cart</h1>
+        </div>
+        </div>
+    )
+}
