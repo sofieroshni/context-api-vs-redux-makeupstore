628fc979167f41a2fc9ccf753ea0c5a06fc179ce
Sofie <sov.002@edu.zealand.dk>
2026-05-23 22:50:36 +0200

    btn i bunden

--- makeupstore-context-api/src/Components/Navbar.jsx
+++ makeupstore-context-api/src/Components/Navbar.jsx
@@ -13,9 +13,10 @@ const Navbar = () => {
         <div className="d-flex justify-content-between bg-secondary p-3 text-white">
             <Link to='/' className="navbar-brand fs-4 fw-bolder">Shop</Link>
             <div className="d-flex gap-4 align-items-center">
+                {/* <span>Penge på konto: {userMoney.toFixed(2)} kr</span> */}
                 <Link to='/cart' className="text-white fs-2 text-decoration-none">
-<BsCart /> {totalItem(cart) || 0} 
-                </Link><br />
+<BsCart /> {totalItem(cart) || 0}
+                </Link>
             </div>
         </div>
         </header>
